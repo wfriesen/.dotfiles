f6ff570b28d5cf850822bbfd2943586695b9e985
William Friesen <wfriesen@gmail.com>
2017-04-26 11:11:16 +1000

    cygwin: diffmerge: deal with /dev/null for new files

--- scripts/diffmerge-wrapper.sh
+++ scripts/diffmerge-wrapper.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
-sgdm $(cygpath -aw "$1") $(cygpath -aw "$2")
+LOCAL=$1
+REMOTE=$2
+NULL_FILE=/tmp/diffmerge-wrapper-fake-dev-null
+
+if [ ! -f "$NULL_FILE" ]; then
+  touch "$NULL_FILE"
+fi
+
+if [ "$LOCAL" == '/dev/null' ]; then
+  LOCAL=$NULL_FILE
+fi
+if [ "$REMOTE" == '/dev/null' ]; then
+  REMOTE=$NULL_FILE
+fi
+
+sgdm $(cygpath -aw "$LOCAL") $(cygpath -aw "$REMOTE")
