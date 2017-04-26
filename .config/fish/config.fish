101787c7dd4a6de8f8d8abb2ad6ce92daaa7fe2e
William Friesen <wfriesen@gmail.com>
2017-04-27 09:20:42 +1000

    fish: set VISUAL to vi/vim
    Conflicts:
    	.config/fish/config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,13 @@ end
 if not type vim 1>/dev/null 2>&1; and type vi 1>/dev/null 2>&1
   alias vim "vi"
 end
+
+if which vi 1>/dev/null 2>&1
+  set VISUAL vi
+else if which vim 1>/dev/null 2>&1
+  set VISUAL vim
+end
+
 alias dit "git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 
 if [ $TERM != screen-256color ]
