6d586e857bdbc1e138e422b02733c5346a5e5905
William Friesen <wfriesen@gmail.com>
2017-04-26 10:42:28 +1000

    Windows vi aliases
    Only attempt to alias vi->vim if needed
    Also do vim->vi if needed

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,10 @@
-alias vi "vim"
+if not type vi 1>/dev/null 2>&1; and type vim 1>/dev/null 2>&1
+  alias vi "vim"
+end
+
+if not type vim 1>/dev/null 2>&1; and type vi 1>/dev/null 2>&1
+  alias vim "vi"
+end
 alias dit "git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 
 if [ $TERM != "screen" ]
