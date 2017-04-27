a14c5458a66a07d9ceb8f4c40b782c21e553495e
William Friesen <wfriesen@gmail.com>
2017-04-27 22:31:35 +1000

    tmux: 256 color support

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,7 @@ if not type vim 1>/dev/null 2>&1; and type vi 1>/dev/null 2>&1
 end
 alias dit "git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 
-if [ $TERM != "screen" ]
+if [ $TERM != screen-256color ]
 	if tmux has-session
 		tmux a
 	else
