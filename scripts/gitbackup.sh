1a8fc70d982444db83d140f0f355d4e8e81fb92b
William Friesen <wfriesen@gmail.com>
2017-05-15 15:33:14 +1000

    Merged backup and restore scripts
    Added some more sanity checking

--- scripts/gitbackup.sh
+++ scripts/gitbackup.sh
@@ -2,45 +2,70 @@
 set -eo pipefail
 
 
-######################
-# Validate arguments #
-######################
-GIT_LOCATION=$1
-BACKUP_ROOT=$2
+OPERATION=$1
+GIT_LOCATION=$2
+BACKUP_LOCATION=$3
 
 function usage()
 {
-  echo "Usage: gitbackup.sh GIT_LOCATION BACKUP_ROOT"
+  echo "Usage: gitbackup.sh backup|restore GIT_LOCATION BACKUP_LOCATION"
 }
 
-if [ -z "$GIT_LOCATION" ] || [ -z "$BACKUP_ROOT" ]; then
+function backup()
+{
+  CURRENT_DATE=`date +%Y-%m-%d-%H-%M-%S`
+  THIS_BACKUP_LOCATION="$BACKUP_LOCATION/$CURRENT_DATE"
+
+  mkdir $THIS_BACKUP_LOCATION
+
+  pushd $GIT_LOCATION
+
+  # Get the HEAD log message, so we know where these changes came from
+  git log -1 HEAD > "$THIS_BACKUP_LOCATION"/HEAD.log
+
+  # Write out a diff of any changes
+  git diff > "$THIS_BACKUP_LOCATION"/diff
+
+  # Tar up any untracked files, so that they can retain their full path
+  git ls-files --others --exclude-standard | \
+    tar -cvf "$THIS_BACKUP_LOCATION"/untracked.tar -T -
+
+  popd
+}
+
+function restore()
+{
+  pushd $GIT_LOCATION
+  DIFF="$BACKUP_LOCATION/diff"
+  UNTRACKED_TAR="$BACKUP_LOCATION/untracked.tar"
+
+  if [ ! -f $DIFF ] || [ ! -f $UNTRACKED_TAR ]; then
+    usage
+    exit 4
+  fi
+
+  git apply $DIFF
+  tar xvf $UNTRACKED_TAR
+
+  popd
+}
+
+
+if [ -z "$GIT_LOCATION" ] || [ -z "$BACKUP_LOCATION" ] || [ -z $OPERATION ]; then
   usage
   exit 1
 fi
 
-if [ ! -d $GIT_LOCATION ] || [ ! -d $BACKUP_ROOT ]; then
+if [ ! -d $GIT_LOCATION ] || [ ! -d $BACKUP_LOCATION ]; then
   usage
   exit 2
 fi
 
-
-##################
-# Perform backup #
-##################
-
-CURRENT_DATE=`date +%Y-%m-%d-%H-%M-%S`
-BACKUP_LOCATION="$BACKUP_ROOT/$CURRENT_DATE"
-
-mkdir $BACKUP_LOCATION
-
-pushd $GIT_LOCATION
-
-# Get the HEAD log message, so we know where these changes came from
-git log -1 HEAD > "$BACKUP_LOCATION"/HEAD.log
-
-# Write out a diff of any changes
-git diff > "$BACKUP_LOCATION"/diff
-
-# Tar up any untracked files, so that they can retain their full path
-git ls-files --others --exclude-standard | \
-  tar -cvf "$BACKUP_LOCATION"/untracked.tar -T -
+if [ $OPERATION = "backup" ]; then
+  backup
+elif [ $OPERATION = "restore" ]; then
+  restore
+else
+  usage
+  exit 3
+fi
