b98b22137a4b6557012692ba7c3c8f6d6c2a52ca
William Friesen <wfriesen@gmail.com>
2017-09-04 20:11:36 +1000

    xmonad: basic xmobar config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -6,4 +6,13 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
 main = do
-    xmonad $ defaultConfig
+    xmproc <- spawnPipe "xmobar"
+
+    xmonad $ docks $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        }
