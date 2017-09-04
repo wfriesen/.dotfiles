6dc626c5eb53bd65bcad89b49814daf2988405ca
William Friesen <wfriesen@gmail.com>
2017-09-04 10:51:04 +1000

    zsh: bind home and end keys

--- .zsh/bindkeys.zsh
+++ .zsh/bindkeys.zsh
@@ -3,3 +3,7 @@ bindkey -M emacs '^P' history-substring-search-up
 bindkey -M emacs '^N' history-substring-search-down
 bindkey \^U backward-kill-line
 bindkey '^[[Z' reverse-menu-complete
+
+# Home and End keys
+bindkey "\e[1~" beginning-of-line
+bindkey "\e[4~" end-of-line
