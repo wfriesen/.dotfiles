8869fdc61c941263c090cb08cc33e7172feefcaa
William Friesen <wfriesen@gmail.com>
2017-10-14 10:34:53 +1100

    xmonad: volume key mappings

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,6 +4,7 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
+import Graphics.X11.ExtraTypes.XF86
 
 main = do
     xmproc <- spawnPipe "xmobar"
@@ -20,4 +21,7 @@ main = do
         `additionalKeys`
         [
           ((mod4Mask, xK_p), spawn "dmenu_run -fn 'Hack' -z")
+        , ((0, xF86XK_AudioRaiseVolume), spawn "amixer set Master 10%+")
+        , ((0, xF86XK_AudioLowerVolume), spawn "amixer set Master 10%-")
+        , ((0, xF86XK_AudioMute), spawn "amixer set Master toggle")
         ]
