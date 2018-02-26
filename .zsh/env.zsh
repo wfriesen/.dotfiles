7bf723b47a9e8fa3a07e0e5a3294e14ca932f61c
William Friesen <wfriesen@gmail.com>
2018-02-26 18:02:12 +1100

    zsh: fix for less options

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -1,5 +1,5 @@
 export LESS="--quit-if-one-screen"
-export LESS="--R $LESS" # Show colors
+export LESS="-R $LESS" # Show colors
 export LESS="-X $LESS" # Don't clear the screen
 export LESS="--LONG-PROMPT $LESS" # Show line numbers currently displayed
 export LESS="--ignore-case $LESS"
