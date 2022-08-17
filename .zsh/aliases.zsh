b449e01abdee48fe5c039a25c814f4377ee78489
William Friesen <wfriesen@gmail.com>
2022-08-17 12:28:44 +1000

    vim: fix dotfiles-update-remotes command after moving plugins

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -37,7 +37,7 @@ dotfiles-update-remotes() {
     | sed -e "s/^\s*url = https:\/\/github\.com\/.*\/\(.*\)\.git/\1/g")
   do
     echo Updating $remotename
-    eval dotfiles subtree pull --prefix .vim/bundle/$remotename vim-$remotename master --squash
+    eval dotfiles subtree pull --prefix .vim/pack/plugins/start/$remotename vim-$remotename master --squash
   done
 
   for remotename in $(awk '/^\[remote "zsh-/ {while ($0 !~ /\s*url\s*=\s*.*/) {getline;} print $0}' .dotfiles/config \
