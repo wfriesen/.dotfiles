27ff3d2bfb5e4d2c88aaa1bad93e908df3a3f353
William Friesen <wfriesen@gmail.com>
2025-07-07 21:55:30 +1000

    rscp: follow symlinks

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -20,7 +20,7 @@ alias cp='cp -i'
 alias mv='mv -i'
 alias pd='popd'
 alias gopen='gio open'
-alias rscp='rsync --archive --partial --progress'
+alias rscp='rsync --copy-links --archive --partial --progress'
 if [[ $OSTYPE == "msys" ]]; then
   alias ssh='ssh -F ~/.ssh/config'
 fi
